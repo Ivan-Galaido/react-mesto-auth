22a0a46d8fb85711facd808cf2795f2015a151c2
Ivan-Galaido <75268958+Ivan-Galaido@users.noreply.github.com>
2021-06-24 15:49:37 +0300

    Delete Header.js

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="header">
-      <a className="header__logo" aria-label="На главную" href="./index.html" target="_self"> </a>
-    </header>
-  );
-}
-
-export default Header;
